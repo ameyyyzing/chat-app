323be9822934de657ed3886fb4adace9487ab690
ameya <pandavameya@gmail.com>
2021-06-17 20:52:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'rsuite/dist/styles/rsuite-default.css'
+
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div>Hello</div>
+  );
+}
+
+export default App;
